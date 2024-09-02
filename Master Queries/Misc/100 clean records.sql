f58c9e85b96a392d33f1564281733c0bc06ad8ec
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-09-02 14:12:56 +0100

    feat: wip group by cleaned name and refactor

--- Master Queries/Misc/100 clean records.sql
+++ Master Queries/Misc/100 clean records.sql
@@ -61,25 +61,25 @@ FROM [ODS].[dbo].[customer] customer
     ON facility_party.source = facility.source
         AND facility_party.facility_ods_key = facility.ods_key
     JOIN [ODS].[dbo].[customer_x_classification__relationship] customer_x_classification__relationship_primind
-        ON customer_x_classification__relationship_primind.customer_ods_key = customer.ods_key
+    ON customer_x_classification__relationship_primind.customer_ods_key = customer.ods_key
         AND customer_x_classification__relationship_primind.ods_key LIKE 'PRIMIND#%'
     JOIN [ODS].[dbo].[customer_classification] customer_classification_primind
-        ON customer_x_classification__relationship_primind.classification_ods_key = customer_classification_primind.ods_key
+    ON customer_x_classification__relationship_primind.classification_ods_key = customer_classification_primind.ods_key
     JOIN [ODS].[dbo].[customer_x_classification__relationship] customer_x_classification__relationship_ind
-        ON customer_x_classification__relationship_ind.customer_ods_key = customer.ods_key
+    ON customer_x_classification__relationship_ind.customer_ods_key = customer.ods_key
         AND customer_x_classification__relationship_ind.ods_key LIKE 'IND#%'
     LEFT JOIN [ODS].[dbo].[customer_classification] customer_classification_ind
-        ON customer_x_classification__relationship_ind.classification_ods_key = customer_classification_ind.ods_key
+    ON customer_x_classification__relationship_ind.classification_ods_key = customer_classification_ind.ods_key
     LEFT JOIN [ODS].[dbo].[customer_x_classification__relationship] customer_x_classification__relationship_indshare
-        ON customer_x_classification__relationship_indshare.customer_ods_key = customer.ods_key
+    ON customer_x_classification__relationship_indshare.customer_ods_key = customer.ods_key
         AND customer_x_classification__relationship_indshare.ods_key LIKE 'INDSHARE#%'
     LEFT JOIN [ODS].[dbo].[customer_classification] customer_classification_indshare
-        ON customer_x_classification__relationship_indshare.classification_ods_key = customer_classification_indshare.ods_key
+    ON customer_x_classification__relationship_indshare.classification_ods_key = customer_classification_indshare.ods_key
     JOIN [ODS].[dbo].[customer_x_classification__relationship] customer_x_classification__relationship_cit
-        ON customer_x_classification__relationship_cit.customer_ods_key = customer.ods_key
+    ON customer_x_classification__relationship_cit.customer_ods_key = customer.ods_key
         AND customer_x_classification__relationship_cit.ods_key LIKE 'CITIZENCLASS#%'
     JOIN [ODS].[dbo].[customer_classification] customer_classification_cit
-        ON customer_x_classification__relationship_cit.classification_ods_key = customer_classification_cit.ods_key
+    ON customer_x_classification__relationship_cit.classification_ods_key = customer_classification_cit.ods_key
     JOIN [ODS].[dbo].[customer_risk_rating]
     ON customer_risk_rating.customer_ods_key = customer.ods_key
     JOIN [ODS].[dbo].[customer_address]
@@ -133,6 +133,7 @@ WHERE customer.source = 'ACBS'
 GROUP BY
     customer.customer_name,
     sf_customer.customer_code,
+    cleaned_names.cleaned_name,
     customer.customer_party_unique_reference_number,
     customer.customer_type_description,
     customer.customer_watch_monitor_flag,
@@ -146,7 +147,6 @@ GROUP BY
     customer_risk_rating.customer_credit_risk_rating_datetime,
     customer_x_classification__relationship_primind.customer_classification_relationship_weighting,
     customer_risk_rating.customer_risk_rating_type_description,
-    cleaned_names.cleaned_name,
     customer_risk_parameter_pod.customer_risk_parameter_value,
     customer_risk_parameter_lgd.customer_risk_parameter_value
 HAVING COUNT(cleaned_names.cleaned_name) = 1
